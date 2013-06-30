45b0ff90017966754c57f19c1237364f0c657368
JRP <jamesroypoulter@gmail.com>
2013-06-30 19:28:49 +0100

    functionality correct

--- app.rb
+++ app.rb
@@ -3,10 +3,39 @@ require "sinatra/reloader"
 require "pg"
 require "haml"
 
+
+# Sessions
 configure do
+  set :environment, "development"
   enable :sessions
+  enable :reload_templates
 end
 
+get '/login' do
+  haml :login
+end
+
+post '/admin_redirect' do
+  if params[:login]=="admin" and params[:password]=="1234"
+    session[:admin] = true
+    redirect "/admin_page"
+  else
+    redirect "/"
+  end
+end
+
+get '/admin_page' do
+  if session[:admin] == true
+    haml :admin_page
+    # @posts = @db.exec "SELECT * FROM unbubble"
+    # haml :index
+  else
+    redirect "/"
+  end
+end
+
+
+# Connecting to database
 before do
 	@db ||= PG.connect(dbname: "unbubble")
 end
